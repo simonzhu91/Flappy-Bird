04edd47615ff6a6ddac24f206160860ceee707f6
KAKA <kaka@KAKAs-MacBook-Pro.local>
2015-05-16 22:10:08 -0700

    Initial Commit

---
+++ Flappy Bird Tutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flappy Bird Tutorial
+//
+//  Created by KAKA on 5/16/15.
+//  Copyright (c) 2015 Simon Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Flappy Bird TutorialTests/Flappy_Bird_TutorialTests.m
@@ -0,0 +1,40 @@
+//
+//  Flappy_Bird_TutorialTests.m
+//  Flappy Bird TutorialTests
+//
+//  Created by KAKA on 5/16/15.
+//  Copyright (c) 2015 Simon Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Flappy_Bird_TutorialTests : XCTestCase
+
+@end
+
+@implementation Flappy_Bird_TutorialTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
